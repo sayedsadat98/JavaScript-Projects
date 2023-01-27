5136dd0ba585c497fa56fdb42f2028c101be6b52
Sayed Us Sadat <sayedsadat98@gmail.com>
2023-01-27 01:09:48 -0500

    logic changes

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,61 +1,54 @@
 'use strict';
 
-let guess = Math.trunc(Math.random() * 20);
-console.log(guess);
+let num = Math.trunc(Math.random() * 20) + 1;
+console.log(num);
+let getScoreText = document.querySelector('.score');
+let scoreMessage = document.querySelector('.message');
+let highScore = document.querySelector('.highscore').textContent;
 
-const displayMessage = document.querySelector('.message');
-let currentScore = document.querySelector('.score').textContent;
-const displayScore = document.querySelector('.score');
-
-let maxScore = -1;
-
-let calculateScore = () => {
-  if (currentScore > 0) {
-    --currentScore;
-    return currentScore;
-  } else {
-    gameOver();
+function checkGameStatus(score) {
+  if (score < 1) {
+    document.querySelector('body').style.backgroundColor = 'red';
+    scoreMessage.textContent = '😣 You Lost The Game!';
   }
-};
+}
 
-function resetGame() {
-  guess = Math.trunc(Math.random() * 20);
+function checkBtn() {
+  let guess = document.querySelector('.guess').value;
+  let score = getScoreText.textContent;
   console.log(guess);
-  document.querySelector('.number').textContent = '?';
-  displayScore.textContent = 20;
-  currentScore = 20;
-  document.querySelector('body').style.backgroundColor = '#222';
-  displayMessage.textContent = 'Start guessing...';
-}
-
-function gameOver() {
-  document.querySelector('body').style.backgroundColor = 'red';
-  displayMessage.textContent = 'Game Over';
-  document.querySelector('.score').value = '0';
-}
-
-function game() {
-  let answer = document.querySelector('.guess').value;
-
-  if (answer > guess) {
-    displayMessage.textContent = 'Too high';
-    displayScore.textContent = calculateScore();
-  } else if (answer < guess) {
-    displayMessage.textContent = 'Too low';
-    displayScore.textContent = calculateScore();
-  } else {
-    displayMessage.textContent = 'You won!!!!';
-    document.querySelector('.number').textContent = answer;
-    document.querySelector('body').style.backgroundColor = 'green';
-
-    if (currentScore > maxScore) {
-      maxScore = currentScore;
+  if (guess == num) {
+    document.querySelector('.number').textContent = num;
+    if (score > highScore) {
+      document.querySelector('.highscore').textContent = score;
     }
 
-    document.querySelector('.highscore').textContent = maxScore;
+    scoreMessage.textContent = '💥Congratulations!';
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '25%';
+  } else if (guess < num) {
+    score--;
+    getScoreText.textContent = score;
+    scoreMessage.textContent = '📉 Too Low!';
+    checkGameStatus(score);
+  } else {
+    score--;
+    getScoreText.textContent = score;
+    scoreMessage.textContent = '📈 Too High!';
+    checkGameStatus(score);
   }
 }
 
-document.querySelector('.check').addEventListener('click', game);
+function againEvent() {
+  num = Math.trunc(Math.random() * 20) + 1;
+  console.log('NEW NUM:' + num);
+  getScoreText.textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  scoreMessage.textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = 'black';
+  document.querySelector('.number').style.width = '15%';
+}
 
-document.querySelector('.again').addEventListener('click', resetGame);
+document.querySelector('.check').addEventListener('click', checkBtn);
+
+document.querySelector('.again').addEventListener('click', againEvent);
